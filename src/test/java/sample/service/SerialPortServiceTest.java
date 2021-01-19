b251b06675bb21053dd0c8ec9f22d4f53dfaeee9
LTJ <597432232@qq.com>
2021-01-19 10:02:55 +0800

    找到设备的一个坑，输入指令不能太快

--- src/test/java/sample/service/SerialPortServiceTest.java
+++ src/test/java/sample/service/SerialPortServiceTest.java
@@ -1,7 +1,11 @@
 package sample.service;
 
 
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
+import gnu.io.UnsupportedCommOperationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import sample.can.CanListener;
@@ -10,19 +14,24 @@ import sample.support.PortParam;
 import sample.util.HexUtils;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.TooManyListenersException;
 
 import static org.junit.Assert.*;
 
 /**
+ * 找到一个困扰很久的bug， 这固件不能连续接收指令，需要程序停顿一定间隔。
  * @author LTJ
  * @version 1.0
  * @date 2021/1/18 18:29
  */
 
 public class SerialPortServiceTest {
-    public static void main(String[] args) throws IOException, TooManyListenersException {
+    public static void main(String[] args)
+            throws IOException, TooManyListenersException, NoSuchPortException, UnsupportedCommOperationException,
+                   PortInUseException {
         SerialPortService serialPortService=new SerialPortService();
         PortParam portParam = new PortParam();
         portParam.setPortName("COM12");
@@ -31,53 +40,52 @@ public class SerialPortServiceTest {
         portParam.setDataBits(PortParam.DATABITS_8);
         portParam.setStopBits(PortParam.STOPBITS_1);
         portParam.setParity(PortParam.PARITY_NONE);
-        AgxResult agxResult = serialPortService.openSerialPort(portParam);
-        if(agxResult.getCode().equals(200)){
-            System.out.println("打开串口成功");
-        }
-        else{
-            System.out.println("打开串口成功");
-            return;
-        }
-        SerialPort theSerialPort = serialPortService.getTheSerialPort();
+        CommPortIdentifier theCommPortIdentifier = CommPortIdentifier.getPortIdentifier(portParam.getPortName());
+        SerialPort theSerialPort = theCommPortIdentifier.open("轨迹测量", 3000);
+        theSerialPort.setSerialPortParams(portParam.getBauldRate(),portParam.getDataBits(),portParam.getStopBits(),portParam.getParity());
+
+        theSerialPort.addEventListener(new CanListener(theSerialPort));
+        theSerialPort.notifyOnDataAvailable(true);
+
+
+//        try {
+//            serialPortService.addEventListener(new CanListener(theSerialPort));
+//            theSerialPort.notifyOnDataAvailable(true);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (TooManyListenersException e) {
+//            e.printStackTrace();
+//        }
+
+
         new Thread(() -> {
             try {
-                serialPortService.addEventListener(new CanListener(theSerialPort));
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (TooManyListenersException e) {
-                e.printStackTrace();
-            }
-            theSerialPort.notifyOnDataAvailable(true);
-        });
-
-        new Thread(() -> {
-            byte[] bytes = HexUtils.hexString2bytes("aa55f50f2804555555550000000000000000000000000000000000000000000000000000000000fe");
-            try {
-                serialPortService.writeData(bytes, 0, bytes.length);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-
-        new Thread(() -> {
+                OutputStream outputStream = theSerialPort.getOutputStream();
+                byte[] bytes = HexUtils.hexString2bytes("aa55f50f2804555555550000000000000000000000000000000000000000000000000000000000fe");
+                System.out.print("\n输入指令:");
+                Arrays.stream(HexUtils.bytesToHexStrings(bytes, 0, bytes.length)).forEach(System.out::print);
+                outputStream.write(bytes,0,bytes.length);
+//                outputStream.flush();
+                Thread.sleep(10);
             byte[] bytes2 = HexUtils.hexString2bytes("aa55f30f2800000000000000000000000000000000000000000000000000000000000000000000fe");
-            try {
-                serialPortService.writeData(bytes2, 0, bytes2.length);
-            } catch (IOException e) {
+                System.out.print("\n输入指令:");
+                Arrays.stream(HexUtils.bytesToHexStrings(bytes2, 0, bytes2.length)).forEach(System.out::print);
+                outputStream.write(bytes2,0,bytes2.length);
+            } catch (Exception  e) {
                 e.printStackTrace();
             }
-        });
+        },"写线程").start();
 
 
 
 
 
-        System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+
+
+
+        System.out.println("\nThread.currentThread().getName() = " + Thread.currentThread().getName());
         System.out.println("按任意键退出");
+        while (true);
 
     }
-
-
 }
